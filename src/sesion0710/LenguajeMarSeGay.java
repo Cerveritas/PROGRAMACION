6c7145432e9f8cba351f09b8bb033f200a1ef313
SergioCerveraJ <seceji@gmail.com>
2022-10-07 14:10:40 +0200

    subida 07/10

---
+++ src/sesion0710/LenguajeMarSeGay.java
@@ -0,0 +1,40 @@
+package sesion0710;
+
+public class LenguajeMarSeGay {
+
+    public static void main(String[] args) {
+
+
+
+        System.out.printf("\033[96m      ██████████");
+        System.out.printf("\033[34m      ██████████\n");System.out.printf("\033[34m      ██████████\n");
+        System.out.printf("\033[34m      ██████████\n");
+        System.out.printf("\033[34m      ██████████\n");
+        System.out.printf("\033[34m      ██████████\n");
+        System.out.printf("\033[34m      ██████████\n");
+        System.out.printf("\033[37m      ██████████\n");//10
+        System.out.printf("\033[37m      ██████████\n");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+}
