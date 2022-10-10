c4f2239ba2a6d742da739b2fe8eaa229ee7b44fb
cerve <seceji@gmail.com>
2022-10-10 19:22:11 +0200

    terminar trabajo casa

--- src/sesion0710/LenguajeMarSeGay.java
+++ src/sesion0710/LenguajeMarSeGay.java
@@ -1,25 +1,61 @@
 package sesion0710;
 
+import java.util.Scanner;
+
+/* Este programa trata de que vamos a dejar que el usuario decida el personaje a escoger.
+*  Tendra 3 opciones = 3 numeros.
+*  1 para Steve, 2 para Mario y 3 Para tu abuela.
+*  Una vez elijamos saldra una imagen de nuestro personaje escogido.
+*  Si metemos un numero fuera del rango indicado saldra una mensaje de error.
+ */
+
 public class LenguajeMarSeGay {
 
     public static void main(String[] args) {
 
+        // declaramos y inicializamos la variable personaje
+        int personajes =0;
 
+        // solicitud y lectura del numero del personaje
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduzca el personaje que quiere usar;");
+        System.out.println("Pulse 1 para seleccionar a STEVE");
+        System.out.println("Pulse 2 para seleccionar a MARIO");
+        System.out.println("Pulse 3 para seleccionar a tu abuela"); //corregir al finalizar
+        personajes = sc.nextInt();
 
-        System.out.printf("\033[96m      ██████████");
-        System.out.printf("\033[34m      ██████████\n");System.out.printf("\033[34m      ██████████\n");
-        System.out.printf("\033[34m      ██████████\n");
-        System.out.printf("\033[34m      ██████████\n");
-        System.out.printf("\033[34m      ██████████\n");
-        System.out.printf("\033[34m      ██████████\n");
-        System.out.printf("\033[37m      ██████████\n");//10
-        System.out.printf("\033[37m      ██████████\n");
-
-
-
-
-
-
+        // determina el personaje escogido
+        if (personajes == 1)
+        {
+            System.out.printf("\033[36m  ██████████  \n");
+            System.out.printf("\033[36m  █        █  \n");
+            System.out.printf("\033[36m  █        █  \n");
+            System.out.printf("\033[36m  ██████████  \n");
+        }
+        else if (personajes == 2)
+        {
+            System.out.printf("\033[35m      ██      \n");
+            System.out.printf("\033[35m     █  █     \n");
+            System.out.printf("\033[35m    █    █    \n");
+            System.out.printf("\033[35m   █      █   \n");
+            System.out.printf("\033[35m  █        █  \n");
+            System.out.printf("\033[35m  ██████████  \n");
+        }
+        else if (personajes == 3)
+        {
+            System.out.printf("\033[32m      ██      \n");
+            System.out.printf("\033[32m     █  █     \n");
+            System.out.printf("\033[32m    █    █    \n");
+            System.out.printf("\033[32m   █      █   \n");
+            System.out.printf("\033[32m   █      █   \n");
+            System.out.printf("\033[32m    █    █    \n");
+            System.out.printf("\033[32m     █  █     \n");
+            System.out.printf("\033[32m      ██      \n");
+        }
+        else
+        {
+            System.out.println("No has introducido un numero correcto, intentelo de nuevo");
+        }
 
 
 
@@ -33,6 +69,8 @@ public class LenguajeMarSeGay {
 
 
     }
+
+
 
 
 
