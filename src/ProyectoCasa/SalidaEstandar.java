e61e4e7fa8474ad0ce501e8e5b5ccdb45fdd1efb
cerve <seceji@gmail.com>
2022-10-15 12:55:03 +0200

    casa 1510

---
+++ src/ProyectoCasa/SalidaEstandar.java
@@ -0,0 +1,51 @@
+package ProyectoCasa;
+
+public class SalidaEstandar {
+
+    public static void main(String[] args) {
+
+
+            System.out.println("\033[31m ▅▅▅▅▅▅▅▅▅");
+            System.out.println("\033[31m    ▅▅▅");
+            System.out.println("\033[31m    ▅▅▅");
+            System.out.println("\033[31m    ▅▅▅");
+            System.out.println("\033[31m    ▅▅▅");
+            System.out.println("  ");
+            System.out.println("\033[31m ▅▅▅▅▅▅▅▅▅");
+            System.out.println("\033[31m ▅▅▅");
+            System.out.println("\033[31m ▅▅▅▅▅");
+            System.out.println("\033[31m ▅▅▅");
+            System.out.println("\033[31m ▅▅▅▅▅▅▅▅▅");
+            System.out.println("  ");
+            System.out.println("  ");
+            System.out.println("  ");
+            System.out.println("\033[31m ▅▅▅▅▅▅▅▅▅");
+            System.out.println("\033[31m ▅▅     ▅▅");
+            System.out.println("\033[31m ▅▅▅▅▅▅▅▅▅");
+            System.out.println("\033[31m ▅▅     ▅▅");
+            System.out.println("\033[31m ▅▅     ▅▅");
+            System.out.println("  ");
+            System.out.println("\033[31m ▅▅▅   ▅▅▅");
+            System.out.println("\033[31m ▅▅▅▅▅▅▅▅▅");
+            System.out.println("\033[31m ▅▅▅ ▅ ▅▅▅");
+            System.out.println("\033[31m ▅▅     ▅▅");
+            System.out.println("\033[31m ▅▅     ▅▅");
+            System.out.println("  ");
+            System.out.println("\033[31m ▅▅▅▅▅▅▅▅▅");
+            System.out.println("\033[31m ▅▅     ▅▅");
+            System.out.println("\033[31m ▅▅     ▅▅");
+            System.out.println("\033[31m ▅▅     ▅▅");
+            System.out.println("\033[31m ▅▅▅▅▅▅▅▅▅");
+
+
+
+
+    }
+
+
+
+
+
+
+
+}
--- src/sesion1310/EjercicioGeneraciones.java
+++ src/sesion1310/EjercicioGeneraciones.java
@@ -11,25 +11,35 @@ public class EjercicioGeneraciones {
         System.out.println("Indica de que año eres y te dire a que generacion pertences");
         año=sc.nextInt();
 
-        if (año>=1994 && año<=2010 )
+          if (año>=1994 && año<=2010 )
         {
             System.out.println("Generacion Z");
         }
 
-         while (año>=1981 && año<=1993)
+          else if (año>=1981 && año<=1993)
         {
             System.out.println("Generacion Y");
-            {break;}
         }
-         while (año>=1969 && año<=1980)
+          else if (año>=1969 && año<=1980)
         {
             System.out.println("Generacion X");
-            {break;}
         }
-
-
-
-
+          else if (año>=1949 && año<=1968)
+        {
+             System.out.println("Generacion X");
+         }
+          else if (año>=1930 && año<=1948)
+         {
+             System.out.println("Generacion X");
+         }
+          else
+          {
+            System.out.println("Error");
+          }
+        while (año<1948 && año>2010);
+        {
+            System.out.println("Tienes que volver a empezar y meter un año correcto");
+        }
     }
 
 }
