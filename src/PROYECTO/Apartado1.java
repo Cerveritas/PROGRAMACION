7f8e0e7e60c90b61b7b3e1b83c2d93a2ec2b0576
cerve <seceji@gmail.com>
2022-10-18 19:25:07 +0200

    casa 1810

--- src/PROYECTO/Apartado1.java
+++ src/PROYECTO/Apartado1.java
@@ -3,6 +3,12 @@ package PROYECTO;
 import java.util.Scanner;
 
 public class Apartado1 {
+    /*
+    * PROGRAMACION
+    * UNIDAD 1 Y 2
+    * @author Sergio Cervera Jimenez
+    * @curse 1ºM DAM+
+     */
     public static void main(String[] args) {
 
     System.out.print    ("\033[36m ############################################################################\n");
@@ -17,11 +23,13 @@ public class Apartado1 {
 
     int trabajo = 0;
 
+
     Scanner sc = new Scanner(System.in);
     System.out.println("\033[33m Para responder SI, pulse 1.");
     System.out.println("\033[33m Para responder NO, pulse 2.");
     System.out.println("\033[32m ¿Se encuentra ahora en un puesto de trabajo?");
     trabajo = sc.nextInt();
+
 
     if (trabajo == 1)
     {
@@ -34,9 +42,35 @@ public class Apartado1 {
 
     System.out.println(" Vamos a la segunda pregunta");
     System.out.println(" ");
-
+//a partir de aqui se debera decidir si vale esta forma de pedir los datos al usuario
     System.out.println("\033[33m El dinero que desea adqurir debe ser igual o menor al 80% del precio de la vivienda.");
-    System.out.println("\033[32m ¿Cuanto dinero deseas solicitar?");
+
+    int precio=0;
+    Scanner pr = new Scanner(System.in);
+    System.out.println("\033[32m ¿Cuanto vale la vivienda que desea comprar?");
+    precio = pr.nextInt();
+
+    int porcentaje;
+    porcentaje=precio/100*80;
+
+    System.out.println("\033[33m PUEDES PEDIR UN PRESTAMO DE  "+porcentaje+ "O INFERIOR");
+    System.out.println(" ");
+
+    int user=0;
+    Scanner us = new Scanner(System.in);
+    System.out.println("\033[32m ¿Cuanto deseas pedir?");
+    user = us.nextInt();
+
+    if (user > porcentaje)
+    {
+        System.out.println("\033[31m El dinero solicitado es superior a lo indicado, vuelvalo a intentar");
+    }
+    else if (user <= porcentaje)
+    {
+        System.out.println("\033[37m Perfecto usted a decidido pedir un prestamo de "+user);
+    }
+
+
 
 
 
