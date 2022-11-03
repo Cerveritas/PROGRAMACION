0998a4e4ad415ab8bcbaf6ec11063ccf2a074db2
SergioCerveraJ <seceji@gmail.com>
2022-11-03 12:30:46 +0100

    sesion 03/11

---
+++ src/sesion0311/Simulacion.java
@@ -0,0 +1,32 @@
+package sesion0311;
+/*
+
+import java.util.Scanner;
+
+public class Simulacion {
+    public static void main(String[] args) {
+
+        System.out.println("Introduzca dos valores");
+        System.out.println(" ");
+        Scanner sc = new Scanner(System.in);
+        int x1,x2;
+        System.out.println("Introduzca primer valor");
+        x1 = sc.nextInt();
+        System.out.println("Introduzca segundo valor");
+        x2 = sc.nextInt();
+
+        int numero=0;
+        switch (numero){
+            case 1: {System.out.println(Calculadora.obtenerSuma(Calculadora.suma));break;}
+            case 2: {System.out.println(resta.calculadora);
+
+        }
+
+
+
+
+
+
+    }
+}
+*/
