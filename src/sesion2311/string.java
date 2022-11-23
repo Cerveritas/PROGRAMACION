d0e55f27a780845ae35e62d87f8f1c8d0136a100
seceji@gmail.com <seceji@gmail.com>
2022-11-23 17:46:33 +0100

    casa 2311

--- src/sesion2311/string.java
+++ src/sesion2311/string.java
@@ -5,7 +5,9 @@ public class string {
     public static void main(String[] args) {
 
         String nombre = "Sergio";
-        char[] array = {'c','e','r','v','e','r','a'};
+        char[] array = {'C','e','r','v','e','r','a'};
+        String apellido = new String(array);
+        System.out.println("Mi nombre es "+nombre+" y mi apellido es "+apellido);
 
 
 
