7cb58052c340f6d6308276fa853c0f790bd0d7ff
SergioCerveraJ <seceji@gmail.com>
2022-11-23 10:21:31 +0100

    sesion 23/11

---
+++ src/sesion2311/string.java
@@ -0,0 +1,17 @@
+package sesion2311;
+
+public class string {
+
+    public static void main(String[] args) {
+
+        String nombre = "Sergio";
+        char[] array = {'c','e','r','v','e','r','a'};
+
+
+
+
+
+    }
+
+
+}
