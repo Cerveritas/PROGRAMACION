2e8106d83575b9f54a7334c97368695375af9161
seceji@gmail.com <seceji@gmail.com>
2022-12-08 18:12:01 +0100

    casa 8/12

--- src/EjerciciosMetodos/Ejercicio6.java
+++ src/EjerciciosMetodos/Ejercicio6.java
@@ -1,4 +1,4 @@
-/*package EjerciciosMetodos;
+package EjerciciosMetodos;
 
 import java.util.Scanner;
 
@@ -8,6 +8,10 @@ public class Ejercicio6 {
 
     public static void main(String[] args) {
 
+        int opcion = 0;
+        int[] numeros = new int[9];
+    do {
+        Scanner sc = new Scanner(System.in);
         System.out.println("Que es lo que quieres hacer: ");
         System.out.println(" ");
         System.out.println("(1) - Generar un array");
@@ -17,81 +21,90 @@ public class Ejercicio6 {
         System.out.println("(5) - Ver si un numero esta en el array o no");
         System.out.println("(6) - Ver en que posicion se encuentra un numero");
         System.out.println("(7) - Copiar el array en otro array");
-
-
-        int opcion = 0;
-        Scanner sc = new Scanner(System.in);
         opcion = sc.nextInt();
 
 
 
-        switch (opcion)
-        {
-            case 1: numeros=generarArray(int[]numeros);
+        switch (opcion) {
+            case 1:
+                numeros = generarArray(numeros);
+                System.out.println("Array creado correctamente...");
+                break;
 
-                }
+
+            case 2:System.out.println("El numero minimo es "+devolverMinimo(numeros));break;
 
 
-        //    case 2:numeros=devolverMinimo(numeros);break;
 
-        //    case 3:numeros=devolverMaximo(numeros);break;
 
-        //    case 4:numeros=devolverMedia(numeros);break;
+            //case 3:numeros=devolverMaximo(numeros);break;
 
-        //    case 5:verSiNumeroEsta(numeros);break;
+            case 4:System.out.println("La media de los numeros del array es: "+devolverMedia(numeros));
+                break;
 
-        //    case 6:numeros=devovlerPosicion(numeros);break;
+            //case 5:verSiNumeroEsta(numeros);break;
 
-        //    case 7:numeros=copiarArray(numeros);break;
+            //case 6:numeros=devovlerPosicion(numeros);break;
 
-               // default:System.out.println("El numero introducido no es correcto.");break;
+            //case 7:numeros=copiarArray(numeros);break;
+
+            default:
+                System.out.println("El numero introducido no es correcto.");
+                break;
 
         }
-
+    } while (opcion != 0) ;
 
 
     }
 
     // METODO 1 --> Genera un array
-    public static int[] generarArray(int numeros){
-        Scanner stc = new Scanner(System.in);
-        int[] array;
-        System.out.println("Intruzca la dimension del array");
-        int dimension = stc.nextInt();
-        array = new int[dimension];
-        for (int i=0; i<array.length; i++){
-            System.out.println("Introduzca el valor "+i);
-            array[i] = stc.nextInt();
-            return array;
+    public static int[] generarArray(int[] numeros){
+        numeros=new int[9];
+        Scanner sc = new Scanner(System.in);
 
-
+        for(int i=0; i<9; i++){
+            System.out.println("Introduce el nuemero"+i);
+            numeros[i]= sc.nextInt();
+        }
+        return numeros;
 
     }
 
 
-    //public static int[] devolverMinimo(int[]numeros){
+    public static int[] devolverMinimo(int[]numeros){
+        int minNumero = numeros[0];
+        for (int i=0; i<9; i++){
+            if (minNumero>numeros[i]){
+                minNumero=numeros[i];
+                minNumero=i;
+
+
+            }
+        }
+        return numeros;
+    }
+
+    //public static int[] devolverMaximo(int[]numeros){
+
   //  }
 
-  //  public static int[] devolverMaximo(int[]numeros){
+    public static int devolverMedia(int[]numeros){
+        return (numeros[0]+numeros[1]+numeros[2]+numeros[3]+numeros[4]+numeros[5]+numeros[6]+numeros[7]+numeros[8])/9;
 
-  //  }
+    }
 
-  //  public static int[] devolverMedia(int[]numeros){
-
-  //  }
-
-  //  public static boolean[] verSiNumeroEsta(int[]numeros){
+   // public static boolean[] verSiNumeroEsta(int[]numeros){
 
   //  }
 
   //  public static int[] devovlerPosicion(int[]numeros){
-
-  //  }
+//
+//    }
 
  //   public static int[] copiarArray(int[]numeros){
 
-   // }
+ //   }
 
 
 }
-*/
