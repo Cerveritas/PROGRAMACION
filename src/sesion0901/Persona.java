7d8ecd6ae13a78b09d0d160de8e565ae329186ae
SergioCerveraJ <seceji@gmail.com>
2023-01-11 10:19:02 +0100

    sesion 1101

--- src/sesion0901/Persona.java
+++ src/sesion0901/Persona.java
@@ -5,9 +5,9 @@ public class Persona {
     //atributos
 
     private String dni;
-    private String nombre="Sergio";
+    private String nombre;
     private String apellidos;
-
+    private int edad;
 
             // la inicializacion de las variables se puede hacer tanto
             // en los atributos como en los metodos
@@ -19,11 +19,32 @@ public class Persona {
 
     }
 
-    //get
+    //get y set de DNI
     public String getDni(){
         return dni;
     }
-
-
-
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+    //get y set de NOmbre
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    //get y set de Apellidos
+    public String getApellidos() {
+        return apellidos;
+    }
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+    //get y set de edad
+    public int getEdad() {
+        return edad;
+    }
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
 }
